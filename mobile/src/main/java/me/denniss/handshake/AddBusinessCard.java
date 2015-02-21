5c1e51bc7ed2b32840c250bccad2c168ebc12526
Tomer Shemesh <tomerjshemesh@gmail.com>
2015-02-21 09:13:43 -0800

    more buiness stuff

--- mobile/src/main/java/me/denniss/handshake/AddBusinessCard.java
+++ mobile/src/main/java/me/denniss/handshake/AddBusinessCard.java
@@ -1,38 +1,99 @@
 package me.denniss.handshake;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 public class AddBusinessCard extends ActionBarActivity {
 
+    int PICK_IMAGE = 1;
+    String imageUri = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_business_card);
+
+        imageUri = "";
+        final EditText businessName = (EditText)findViewById(R.id.businessName);
+        final EditText name = (EditText)findViewById(R.id.name);
+        final EditText email = (EditText)findViewById(R.id.email);
+        final EditText jobTitle = (EditText)findViewById(R.id.jobTitle);
+        final EditText address = (EditText)findViewById(R.id.address);
+        final EditText number = (EditText)findViewById(R.id.phoneNumber);
+        final EditText fax = (EditText)findViewById(R.id.fax);
+        final EditText website = (EditText)findViewById(R.id.website);
+        final Button imageButton = (Button)findViewById(R.id.selectImageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, PICK_IMAGE);
+            }
+        });
+
+        final Button cancel = (Button)findViewById(R.id.businessCardCancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        Button save = (Button)findViewById(R.id.businessCardSave);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BusinessCard card = new BusinessCard();
+                card.businessName = businessName.getText().toString();
+                card.name = name.getText().toString();
+                card.email = email.getText().toString();
+                card.jobTitle = jobTitle.getText().toString();
+                card.address = address.getText().toString();
+                card.number = number.getText().toString();
+                card.fax = fax.getText().toString();
+                card.website = website.getText().toString();
+                card.imageUrl = imageUri;
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_business_card, menu);
-        return true;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK)
+        {
+            Uri selectedImage = data.getData();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            imageUri = getRealPathFromURI(this,selectedImage);
         }
 
-        return super.onOptionsItemSelected(item);
+        super.onActivityResult(requestCode,resultCode,data);
     }
+
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
 }
---
+++ mobile/src/main/java/me/denniss/handshake/BusinessCard.java
@@ -0,0 +1,19 @@
+package me.denniss.handshake;
+
+import java.io.Serializable;
+
+/**
+ * Created by Tomer on 2/21/2015.
+ */
+public class BusinessCard implements Serializable{
+    public static String name;
+    public static String email;
+    public static String businessName;
+    public static String jobTitle;
+    public static String address;
+    public static String number;
+    public static String fax;
+    public static String website;
+    public static String imageUrl;
+
+}
